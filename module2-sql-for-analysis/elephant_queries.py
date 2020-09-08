71f69d7abce93ef07a2f18e8a1ff0fd6d51a1e55
Navroz Lamba <lamba-navroz@lambdastudents.com>
2020-09-09 04:31:02 +0530

    Elephant queries

---
+++ module2-sql-for-analysis/elephant_queries.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv() # LOads content of the env
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+
+print(DB_NAME, DB_HOST, DB_PASSWORD, DB_USER)
+
+exit()
+
+### Connect to ElephantSQL-hosted PostgreSQL
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                        password=DB_PASSWORD, host=DB_HOST)
+### A "cursor", a structure to iterate over db records to perform queries
+c = conn.cursor()
+### An example query
+c.execute('SELECT * from test_table;')
+### Note - nothing happened yet! We need to actually *fetch* from the cursor
+print(c.fetchall())
